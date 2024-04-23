5a570447332141db867a38b74a3af9781181d8be
Sulwon-0516 <ininin0516@gmail.com>
2024-04-23 13:36:37 +0900

    init

---
+++ js/carousel.js
@@ -0,0 +1,34 @@
+const delay = 3000; //ms
+
+const slides = document.querySelector(".slides");
+const slidesCount = slides.childElementCount;
+const maxLeft = (slidesCount - 1) * 100 * -1;
+
+let current = 0;
+
+function changeSlide(next = true) {
+  if (next) {
+    current += current > maxLeft ? -100 : current * -1;
+  } else {
+    current = current < 0 ? current + 100 : maxLeft;
+  }
+
+  slides.style.left = current + "%";
+}
+
+// let autoChange = setInterval(changeSlide, delay);
+// const restart = function() {
+//   clearInterval(autoChange);
+//   autoChange = setInterval(changeSlide, delay);
+// };
+
+// Controls
+document.querySelector(".next-slide").addEventListener("click", function() {
+  changeSlide();
+  restart();
+});
+
+document.querySelector(".prev-slide").addEventListener("click", function() {
+  changeSlide(false);
+  restart();
+});
